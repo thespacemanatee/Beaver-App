dd88c70d297a39ab30ffefdf3b9900c21b7cb67b
Chee Kit <cheekit.chong98@gmail.com>
2020-11-02 03:20:22 +0800

    Made UI more immersive for phones with display cutout

--- app/src/main/java/com/example/beever/MainActivity.java
+++ app/src/main/java/com/example/beever/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         //Animations for logo
         Animation topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
