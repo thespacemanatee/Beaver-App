8e63e70e513074e3505f49b95fa8accfdba97bc8
Chee Kit <cheekit.chong98@gmail.com>
2020-11-02 02:31:42 +0800

    Added onBoarding files (forgot to git add rip)

---
+++ app/src/main/java/com/example/beever/OnBoardingFragment3.java
@@ -0,0 +1,31 @@
+package com.example.beever;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class OnBoardingFragment3 extends Fragment {
+
+    FloatingActionButton fab;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_onboarding3,container,false);
+        fab = root.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),Login.class);
+                startActivity(intent);
+            }
+        });
+        return root;
+    }
+}
