9073e7bbfd5f2f7d1a362a4d2f8e16f991b72250
Chee Kit <cheekit.chong98@gmail.com>
2020-11-02 12:55:13 +0800

    Added SharedPreferences to show onBoarding once only

--- app/src/main/java/com/example/beever/MainActivity.java
+++ app/src/main/java/com/example/beever/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.beever;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -17,8 +23,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_SCREEN = 3000;
     private static final int NUM_PAGES = 3;
+    private static final int SPLASH_TIMEOUT = 3000;
+    SharedPreferences mSharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         //Animations for logo
         Animation topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
@@ -59,30 +67,47 @@ public class MainActivity extends AppCompatActivity {
         logo.animate().translationY(3400).setDuration(1000).setStartDelay(3000);
         slogan.animate().translationY(3400).setDuration(1000).setStartDelay(3000);
 
-//        Handler handler = new Handler();
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intent = new Intent(MainActivity.this,Login.class);
-//
-//                Pair[] pairs = new Pair[2];
-//                pairs[0] = new Pair<View, String>(image,"logo_image");
-//                pairs[1] = new Pair<View, String>(logo,"logo_text");
-//
-//                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
-//                startActivity(intent, options.toBundle());
-//            }
-//        },SPLASH_SCREEN);
-//
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                MainActivity.this.finish();
-//            }
-//        }, SPLASH_SCREEN+500);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSharedPref = getSharedPreferences("SharedPref",MODE_PRIVATE);
+                boolean isFirstTime = mSharedPref.getBoolean("firstTime",true);
+
+                if (!isFirstTime) {
+
+                    Intent intent = new Intent(MainActivity.this,Login.class);
+
+                    Pair[] pairs = new Pair[2];
+                    pairs[0] = new Pair<View, String>(image,"logo_image");
+                    pairs[1] = new Pair<View, String>(logo,"logo_text");
+
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                    startActivity(intent, options.toBundle());
+
+                }
+            }
+        },SPLASH_TIMEOUT);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSharedPref = getSharedPreferences("SharedPref",MODE_PRIVATE);
+                boolean isFirstTime = mSharedPref.getBoolean("firstTime",true);
+
+                if (isFirstTime) {
+                    SharedPreferences.Editor editor = mSharedPref.edit();
+                    editor.putBoolean("firstTime", false);
+                    editor.commit();
+                } else {
+                    MainActivity.this.finish();
+                }
+            }
+        }, SPLASH_TIMEOUT+1000);
     }
 
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+    private static class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
 
         public ScreenSlidePagerAdapter(@NonNull FragmentManager fm) {
             super(fm);
