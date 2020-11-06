846ba1fa247fcfcbde5cfd0b93ba617cfc687197
Chee Kit <cheekit.chong98@gmail.com>
2020-11-06 19:53:00 +0800

    Updated build.gradle

--- app/src/main/java/com/example/beever/navigation/DashboardFragment.java
+++ app/src/main/java/com/example/beever/navigation/DashboardFragment.java
@@ -1,5 +1,7 @@
 package com.example.beever.navigation;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,14 +17,18 @@ import java.util.Calendar;
 public class DashboardFragment extends Fragment {
 
     int currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-    TextView greeting;
+    private SharedPreferences mSharedPref;
+    TextView greeting, name;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.dashboard_fragment, container, false);
 
         ((NavigationDrawer)getActivity()).getSupportActionBar().setTitle("Dashboard");
+        mSharedPref = this.getActivity().getSharedPreferences("SharedPref", Context.MODE_PRIVATE);
         greeting = root.findViewById(R.id.greeting);
+        name = root.findViewById(R.id.name);
+        name.setText(mSharedPref.getString("registeredName","") + ".");
 
         if (currentTime < 12) {
             greeting.setText(R.string.greetings_morning);
