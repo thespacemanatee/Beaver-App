a1a1dfbaf1f8d58a93e7f7d5f75c9aeba9fc020c
Pillowkoh <72456637+Pillowkoh@users.noreply.github.com>
2020-11-16 21:34:55 +0800

    added some cool calendar

--- app/src/main/java/com/example/beever/feature/CalendarFragment.java
+++ app/src/main/java/com/example/beever/feature/CalendarFragment.java
@@ -10,6 +10,10 @@ import android.view.ViewGroup;
 
 import com.example.beever.R;
 import com.example.beever.navigation.NavigationDrawer;
+import com.shrikanthravi.collapsiblecalendarview.data.Day;
+import com.shrikanthravi.collapsiblecalendarview.widget.CollapsibleCalendar;
+import android.util.Log;
+
 
 public class CalendarFragment extends Fragment {
 
@@ -17,8 +21,48 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_calendar, container, false);
 
         ((NavigationDrawer)getActivity()).getSupportActionBar().setTitle("Calendar");
+        final CollapsibleCalendar collapsibleCalendar = root.findViewById(R.id.calendarView);
+        collapsibleCalendar.setCalendarListener(new CollapsibleCalendar.CalendarListener() {
+            @Override
+            public void onDaySelect() {
+                Day day = collapsibleCalendar.getSelectedDay();
+                Log.i(getClass().getName(), "Selected Day: "
+                        + day.getYear() + "/" + (day.getMonth() + 1) + "/" + day.getDay());
+            }
+
+            @Override
+            public void onItemClick(View view) {
+
+            }
+
+            @Override
+            public void onDataUpdate() {
+
+            }
+
+            @Override
+            public void onMonthChange() {
+
+            }
+
+            @Override
+            public void onWeekChange(int i) {
+
+            }
+
+            @Override
+            public void onDayChanged() {
+
+            }
+
+            @Override
+            public void onClickListener() {
+
+            }
+        });
 
 
         return inflater.inflate(R.layout.fragment_calendar, container, false);
