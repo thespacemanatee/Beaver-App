0c8c54ed2c25dc76eaa3254fb73a7d743773a6c4
Chee Kit <cheekit_chong@mymail.sutd.edu.sg>
2020-11-26 15:40:32 +0800

    Implemented FireStore stuff

--- app/src/main/java/com/example/beever/feature/ToDoFragment.java
+++ app/src/main/java/com/example/beever/feature/ToDoFragment.java
@@ -13,20 +13,26 @@ import android.widget.ExpandableListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.beever.R;
 import com.example.beever.navigation.NavigationDrawer;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ToDoFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
@@ -46,18 +52,21 @@ public class ToDoFragment extends Fragment implements AdapterView.OnItemSelected
 
     private FirebaseAuth fAuth = FirebaseAuth.getInstance();
     private FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+    private String userID;
 
     protected static List<String> ARCHIVED = new ArrayList<>();
     protected static List<String> archivedList = new ArrayList<>();
     protected static HashMap<String, List<String>> expandableListDetail = new HashMap<>();
     protected static ArrayList<String> toDoList = new ArrayList<>();
     protected static ArrayList<String> projectList = new ArrayList<>();
+    protected Map<String, Object> map = new HashMap<>();
     protected int scrollPosition = 0;
 
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle savedInstanceState) {
 
         FirebaseUser fUser = fAuth.getCurrentUser();
+        userID = fUser.getUid();
 
         View rootView = layoutInflater.inflate(R.layout.fragment_to_do, viewGroup, false);
         rootView.setTag(TAG);
@@ -100,12 +109,50 @@ public class ToDoFragment extends Fragment implements AdapterView.OnItemSelected
         });
         Log.d(TAG, FAB);
 
+        populateToDoList();
+
         return rootView;
+    }
+
+    private void populateToDoList() {
+        DocumentReference documentReference = fStore.collection("users").document(userID);
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if (documentSnapshot.exists()) {
+                        projectList = (ArrayList<String>) documentSnapshot.get("groups");
+
+                    }
+                }
+            }
+        });
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         // TODO: get the to do list from firebase according to the project
+        DocumentReference documentReference = fStore.collection("groups").document(parent.getItemAtPosition(position).toString());
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if (documentSnapshot.exists()) {
+                        map = (Map<String, Object>) documentSnapshot.get("todo_list");
+                        for (Map.Entry<String, Object> entry: map.entrySet()) {
+                            if (entry.getKey().equals("current")) {
+//                                toDoList = entry.getValue();
+
+                            }
+                        }
+
+                    }
+                }
+            }
+        });
+
     }
 
     @Override
