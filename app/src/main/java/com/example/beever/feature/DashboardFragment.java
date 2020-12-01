1989a41a3af58eb5e3b323ccadcbfa6db7540e0b
Chee Kit <cheekit.chong98@gmail.com>
2020-12-02 04:27:16 +0800

    Fix bottom menu animation

--- app/src/main/java/com/example/beever/feature/DashboardFragment.java
+++ app/src/main/java/com/example/beever/feature/DashboardFragment.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
