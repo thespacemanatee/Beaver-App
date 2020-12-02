2287f384422673d60fc0084234aef643a34d0696
Pillowkoh <72456637+Pillowkoh@users.noreply.github.com>
2020-12-02 18:57:08 +0800

    Modified CalendarFragment

--- app/src/main/java/com/example/beever/feature/AddEventFragment.java
+++ app/src/main/java/com/example/beever/feature/AddEventFragment.java
@@ -24,9 +24,17 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import com.example.beever.R;
+import com.example.beever.database.EventEntry;
+import com.example.beever.database.UserEntry;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
 
 import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Objects;
 import java.util.TimeZone;
 
@@ -34,16 +42,23 @@ public class AddEventFragment extends Fragment {
 
     private static final String TAG = "AddEventFragment";
 
-    private EditText mInput;
+    private EditText mInput,mDescription;
     private LinearLayout eventStart, eventEnd, addEventButtons;
     private TextView startDateTime, endDateTime;
     private Button cancel, save;
-    private FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+    private Date start, end;
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private UserEntry userEntry;
+    private String USER_ID;
+    private Utils utils;
+    private Calendar calendar = Calendar.getInstance();
+    private int selectedDay, selectedMonth, selectedYear;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_addevent, container, false);
         mInput = root.findViewById(R.id.eventTitle);
+        mDescription = root.findViewById(R.id.description);
         eventStart = root.findViewById(R.id.startEvent);
         startDateTime = eventStart.findViewById(R.id.start_datetime);
         eventEnd = root.findViewById(R.id.endEvent);
@@ -52,23 +67,36 @@ public class AddEventFragment extends Fragment {
         cancel = addEventButtons.findViewById(R.id.cancel_button);
         save = addEventButtons.findViewById(R.id.save_button);
 
+        Bundle bundle = this.getArguments();
+        selectedDay = bundle.getInt("selectedDay");
+        selectedMonth = bundle.getInt("selectedMonth");
+        selectedYear = bundle.getInt("selectedYear");
+
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        USER_ID = firebaseUser.getUid();
+
         final Calendar cldr = Calendar.getInstance();
         int day = cldr.get(Calendar.DAY_OF_MONTH);
         int month = cldr.get(Calendar.MONTH);
         int year = cldr.get(Calendar.YEAR);
 
-        startDateTime.setText(day + "/" + (month + 1) + "/" + year);
-        endDateTime.setText(day + "/" + (month + 1) + "/" + year);
+        start = new GregorianCalendar(selectedYear, selectedMonth, selectedDay).getTime();
+        end = new GregorianCalendar(selectedYear, selectedMonth, selectedDay).getTime();
+
+        startDateTime.setText(selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear);
+        endDateTime.setText(selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear);
 
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG,"onClick: closing dialog");
+                utils = new Utils(v.getContext());
                 String input = mInput.getText().toString();
                 if (input.isEmpty()) {
                     Toast.makeText(getContext(), "No information added. Event not saved.", Toast.LENGTH_SHORT).show();
                 }
+                utils.fadeIn();
                 InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
                 inputMethodManager.hideSoftInputFromWindow(getView().getWindowToken(),0);
                 getFragmentManager().popBackStackImmediate();
@@ -80,11 +108,46 @@ public class AddEventFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: capturing input");
+                utils = new Utils(v.getContext());
                 String input = mInput.getText().toString();
+                String description = mDescription.getText().toString();
                 if (input.isEmpty()){
                     Toast.makeText(getContext(),"No information added. Event not saved.", Toast.LENGTH_SHORT).show();
                     getFragmentManager().popBackStackImmediate();
                 }
+                else{
+                    UserEntry.GetUserEntry getUserEntry = new UserEntry.GetUserEntry(USER_ID,5000) {
+                        @Override
+                        public void onPostExecute() {
+                            userEntry = getResult();
+                            EventEntry eventEntry = new EventEntry();
+                            eventEntry.setName(input);
+                            eventEntry.setDescription(description);
+                            eventEntry.setStart_time(new Timestamp(start));
+                            eventEntry.setEnd_time(new Timestamp(end));
+                            eventEntry.setUser_id_source(USER_ID);
+                            userEntry.modifyEventOrTodo(true, true, true, eventEntry);
+                            UserEntry.SetUserEntry setEvent = new UserEntry.SetUserEntry(userEntry, USER_ID, 5000) {
+                                @Override
+                                public void onPostExecute() {
+                                    Toast.makeText(getContext(),"Event saved successfully.", Toast.LENGTH_SHORT).show();
+                                    getFragmentManager().popBackStackImmediate();
+//                            populateEventsList();
+//                            textEventAdapter.notifyDataSetChanged();
+//                        TextEventAdapter adapter = new TextEventAdapter(list, getContext());
+//                        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL,false);
+//                        RecyclerView mRecyclerView = (RecyclerView) root.findViewById(R.id.recyclerView);
+//                        mRecyclerView.setLayoutManager(linearLayoutManager);
+//                        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+//                        mRecyclerView.setAdapter(adapter);
+                                }
+                            };
+                            setEvent.start();
+                        }
+                    };
+                    getUserEntry.start();
+                }
+                utils.fadeIn();
             }
         });
 
@@ -97,6 +160,10 @@ public class AddEventFragment extends Fragment {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                                 startDateTime.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                calendar.set(year,monthOfYear,dayOfMonth);
+                                start = calendar.getTime();
+
+
                             }
                         }, year, month, day);
                 picker.getDatePicker().setMinDate(cldr.getTimeInMillis());
@@ -113,6 +180,8 @@ public class AddEventFragment extends Fragment {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                                 endDateTime.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                calendar.set(year, monthOfYear, dayOfMonth);
+                                end = calendar.getTime();
                             }
                         }, year, month, day);
                 picker.getDatePicker().setMinDate(cldr.getTimeInMillis());
